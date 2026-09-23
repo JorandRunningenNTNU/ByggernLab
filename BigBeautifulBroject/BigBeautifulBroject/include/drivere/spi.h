d71a103f240782812f7b63925a28690fcf49602d
JorandRunningenNTNU <jorandru@stud.ntnu.no>
2026-09-23 11:15:37 +0200

    spi ferdig?

--- BigBeautifulBroject/BigBeautifulBroject/include/drivere/spi.h
+++ BigBeautifulBroject/BigBeautifulBroject/include/drivere/spi.h
@@ -3,8 +3,7 @@
 typedef enum {IO, Display, CAN} SPIselect;
 
 void setupSPI();
-void selectSlaveSPI(SPIselect slave);
-void writeByteSPI(char data, SPIselect slave);
-void writeSPI(char* data, uint16_t n, SPIselect slave);
-char readByteSPI(SPIselect slave);
-void readSPI(char* data, uint16_t n, SPIselect slave);
+void writeByteSPI(uint8_t data, SPIselect slave);
+void writeSPI(uint8_t* data, uint16_t n, SPIselect slave);
+uint8_t readByteSPI(SPIselect slave);
+void readSPI(uint8_t* data, uint16_t n, SPIselect slave);
--- BigBeautifulBroject/BigBeautifulBroject/source/drivere/spi.c
+++ BigBeautifulBroject/BigBeautifulBroject/source/drivere/spi.c
@@ -6,9 +6,12 @@
 // PB3 Display
 // PB4 IO 
 
+void unSelectAllSlavesSPI();
+void selectSlaveSPI();
+
 void setupSPI(){
 	DDRB |= (1 << 7) | (1 << 5) | (1 << 4) | (1 << 3) | (1 << 1); // PBn som output
-	PORTB |= (1 << 4) | (1 << 3) | (1 << 1); // Setter alle slaveSelect høy, så ingen ingen enheter kobles til
+	unSelectAllSlavesSPI();
 	
 	// valgte innstillinger
 	// MSB sendes først
@@ -20,39 +23,46 @@ void setupSPI(){
 	
 }
 
-void selectSlaveSPI(SPIselect slave){
+void unSelectAllSlavesSPI(){
 	PORTB |= (1 << 4) | (1 << 3) | (1 << 1);
+}
+
+void selectSlaveSPI(SPIselect slave){
+	unSelectAllSlavesSPI();
 	
 	if (slave == CAN){PORTB &= 0xfd;}
 	if (slave == Display){PORTB &= 0xf7;}
 	if (slave == IO){PORTB &= 0xef;}
 }
 
-void writeByteSPI(char data, SPIselect slave){
+void writeByteSPI(uint8_t data, SPIselect slave){
 	selectSlaveSPI(slave);
 	SPDR = data;
 	while(!(SPSR & (1 << 7))){}
+	unSelectAllSlavesSPI();
 }
 
-void writeSPI(char* data, uint16_t n, SPIselect slave){
+void writeSPI(uint8_t* data, uint16_t n, SPIselect slave){
 	// Bruker ikke writeByteSPI, siden den da kaller slaveSelect for hver byte
 	selectSlaveSPI(slave);
-	for(uint16_t i = 0, i<n, i++){
+	for(uint16_t i = 0; i<n; i++){
 		SPDR = data[i];
 		while(!(SPSR & (1 << 7))){}
 	}
+	unSelectAllSlavesSPI();
 }
 
-char readByteSPI(SPIselect slave){
+uint8_t readByteSPI(SPIselect slave){
 	writeByteSPI(0, slave);
 	return SPDR;
 }
 
-void readSPI(char* data, uint16_t n, SPIselect slave){
-	selectSlaveSPI(slave)
-	for(uint16_t i = 0, i<n, i++){
+void readSPI(uint8_t* data, uint16_t n, SPIselect slave){
+	selectSlaveSPI(slave);
+	for(uint16_t i = 0; i<n; i++){
 		SPDR = 0;
 		while(!(SPSR & (1 << 7))){}
 		data[i] = SPDR;
 	}
+	unSelectAllSlavesSPI();
 }
